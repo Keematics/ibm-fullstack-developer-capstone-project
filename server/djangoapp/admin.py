202ea130b880a616c82ca236eff0bb5777e0bcd9
Keematics <agbajeabdulakeems@gmail.com>
2023-06-17 10:26:57 -0400

    added CarModelInline CarMake

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -4,19 +4,22 @@ from .models import CarMake, CarModel
 
 
 # Register your models here.
+
+# CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 6
+
+# CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ['name',  'dealer_id', 'car_type', 'year']
     search_fields = ['name']
 
+# CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
     list_display = ['name', 'description']
     search_fields = ['name']
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
 
 # Register models here
 admin.site.register(CarModel, CarModelAdmin)
